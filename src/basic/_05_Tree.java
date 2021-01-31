76e158afcc0bf89d16f5b78b619276d8f07350b2
jamesfchen <hawks.jamesf@gmail.com>
2021-01-31 21:26:13 +0800

    add 98 tree

--- src/basic/_05_Tree.java
+++ src/basic/_05_Tree.java
@@ -1,5 +1,26 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.TreeSet;
+
 /**
  * tree and binary search tree
  */
 public class _05_Tree {
+    public static void main(String[] args) {
+        //java C++标准库树都是用红黑树
+//        List<Integer> l = Arrays.asList(5, 1, 4, null, null, 3, 6);
+        List<Integer> l = Arrays.asList(5, 1, 4, 3, 6);
+        TreeSet<Integer> ts = new TreeSet<>(l);
+        System.out.println(Arrays.toString(ts.toArray()));
+//        System.out.println("tree first:"+ts.first()+" tree last:"+ts.last());
+//        System.out.println(
+//        ts.tailSet(5)
+//        );
+//        System.out.println(ts.headSet(5));
+    }
+
+//    public List<Integer> inorder(TreeSet<Integer> ts){
+//        if (ts ==null) return Collections.emptyList();
+//    }
 }
---
+++ src/basic/_05_Tree_98.py
@@ -0,0 +1,47 @@
+'''
+98. 验证二叉搜索树
+给定一个二叉树，判断其是否是一个有效的二叉搜索树。
+
+假设一个二叉搜索树具有如下特征：
+
+节点的左子树只包含小于当前节点的数。
+节点的右子树只包含大于当前节点的数。
+所有左子树和右子树自身必须也是二叉搜索树。
+示例 1:
+
+输入:
+    2
+   / \
+  1   3
+输出: true
+示例 2:
+
+输入:
+    5
+   / \
+  1   4
+     / \
+    3   6
+输出: false
+解释: 输入为: [5,1,4,null,null,3,6]。
+     根节点的值为 5 ，但是其右子节点值为 4 。
+'''
+from mock  import TreeNode
+
+class Solution:
+    def inorder(self,root):
+        #O(n)
+        if root is None:
+            return []
+        return self.inorder(root.left) +[root.value]+self.inorder(root.right)
+    def isValidBST1(self, root: TreeNode) -> bool:
+        inorder=self.inorder(root)
+        return inorder == list(sorted(set(inorder)))
+    def isValidBST2(self, root: TreeNode) -> bool:
+        pass
+
+        pass
+if __name__ == "__main__":
+    tree_node = TreeNode.create([2, 1, 3])
+    s = Solution()
+    print(s.isValidBST1(tree_node))
