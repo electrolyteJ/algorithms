16b649d9dcf55e1bd5b5ae22f24ed77490dd1f9d
cjf陈进发 <jf.chen@Ctrip.com>
2021-02-05 18:03:32 +0800

    add 169

--- src/basic/_06_Divide_RecursionAlgorithm_169.py
+++ src/basic/_06_Divide_RecursionAlgorithm_169.py
@@ -4,8 +4,6 @@
 
 你可以假设数组是非空的，并且给定的数组总是存在多数元素。
 
-
-
 示例 1：
 
 输入：[3,2,3]
@@ -20,16 +18,36 @@
 
 尝试设计时间复杂度为 O(n)、空间复杂度为 O(1) 的算法解决此问题。
 '''
+import collections
 
 
 class Solution:
-    def majorityElement(self, nums) -> int:
-        pass
+    def majorityElement1(self, nums) -> int:
+        # 时间复杂度O(n) 空间复杂度O(n)
+        # counts = collections.Counter(nums)
+        counts = dict()
+        for e in nums:
+            counts[e] = counts.get(e, 0) + 1
+        # return max(counts.keys(), key=counts.get)
+        return max(counts.keys(), key=counts.get)
+
+    def majorityElement2(self, nums) -> int:
+        # 时间复杂度O(nlog n) 空间复杂度O(log n)
+        new_nums = sorted(nums)
+        return new_nums[len(nums)//2]
+
+    def majorityElement3(self, nums) -> int:
+        new_nums = sorted(nums)
+        return new_nums[len(nums)//2]
 
 
 if __name__ == "__main__":
     s = Solution()
     l = [3, 2, 3]
-    print('1', s.majorityElement(l))
+    print('1', s.majorityElement1(l))
+    print('2', s.majorityElement2(l))
+    print('3', s.majorityElement3(l))
     l = [2, 2, 1, 1, 1, 2, 2]
-    print('1', s.majorityElement(l))
+    print('1', s.majorityElement1(l))
+    print('2', s.majorityElement2(l))
+    print('3', s.majorityElement3(l))
