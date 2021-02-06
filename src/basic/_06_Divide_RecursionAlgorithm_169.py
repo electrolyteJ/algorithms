10fd1f3094da8758abbc7ff8da3d37824bfb7a11
jamesfchen <hawks.jamesf@gmail.com>
2021-02-06 20:21:03 +0800

    update 169

--- src/basic/_06_Divide_RecursionAlgorithm_169.py
+++ src/basic/_06_Divide_RecursionAlgorithm_169.py
@@ -22,7 +22,23 @@ import collections
 
 
 class Solution:
-    def majorityElement1(self, nums) -> int:
+
+    def majorityElement1(self, nums) -> int:  # 暴力
+        max_value=0
+        majority_e=0
+        #O(n^2)
+        for i in range(len(nums)):
+            e1= nums[i]
+            counter=1
+            for j in range(i+1,len(nums)):
+                e2=nums[j]
+                if e1 ==e2:
+                    counter +=1
+            if counter > max_value:
+                max_value = counter
+                majority_e=e1
+        return majority_e
+    def majorityElement2(self, nums) -> int:
         # 时间复杂度O(n) 空间复杂度O(n)
         # counts = collections.Counter(nums)
         counts = dict()
@@ -31,15 +47,26 @@ class Solution:
         # return max(counts.keys(), key=counts.get)
         return max(counts.keys(), key=counts.get)
 
-    def majorityElement2(self, nums) -> int:
+    def majorityElement3(self, nums) -> int:
         # 时间复杂度O(nlog n) 空间复杂度O(log n)
         new_nums = sorted(nums)
         return new_nums[len(nums)//2]
 
-    def majorityElement3(self, nums) -> int:
-        new_nums = sorted(nums)
-        return new_nums[len(nums)//2]
+    def majorityElement4(self, nums) -> int:#分治
+        def majority_e_rec(lo,hi):
+            if lo==hi :
+                return nums[lo]
+            mid = (hi-lo)//2+lo
+            left = majority_e_rec(lo,mid)
+            right = majority_e_rec(mid+1,hi)
+            if left == right:
+                return left
+            left_count = sum(1 for i in range(lo, hi + 1) if nums[i] == left)
+            right_count = sum(1 for i in range(lo,hi+1) if nums[i] == right)
+            return left if left_count > right_count else right
 
+        #O(nlogn)
+        return majority_e_rec(0,len(nums)-1)
 
 if __name__ == "__main__":
     s = Solution()
@@ -47,7 +74,9 @@ if __name__ == "__main__":
     print('1', s.majorityElement1(l))
     print('2', s.majorityElement2(l))
     print('3', s.majorityElement3(l))
+    print('4', s.majorityElement4(l))
     l = [2, 2, 1, 1, 1, 2, 2]
     print('1', s.majorityElement1(l))
     print('2', s.majorityElement2(l))
     print('3', s.majorityElement3(l))
+    print('4', s.majorityElement4(l))
