712471358d079f0b520d8b227d27757108b85e68
jamesfchen <hawks.jamesf@gmail.com>
2021-02-15 19:44:46 +0800

    add 191 231 338

---
+++ src/basic/_12_BitOp_231.py
@@ -0,0 +1,32 @@
+'''
+231. 2的幂
+给定一个整数，编写一个函数来判断它是否是 2 的幂次方。
+
+示例 1:
+
+输入: 1
+输出: true
+解释: 20 = 1
+示例 2:
+
+输入: 16
+输出: true
+解释: 24 = 16
+示例 3:
+
+输入: 218
+输出: false
+'''
+
+
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        pass
+if __name__=='__main__'
+    s = Solution()
+    n=1
+    print('1', s.isPowerOfTwo(n))
+    n=16
+    print('1', s.isPowerOfTwo(n))
+    n=218
+    print('1', s.isPowerOfTwo(n))
