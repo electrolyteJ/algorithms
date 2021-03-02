00973fc26b1897e345f25d5804fcdc317552aee0
jamesfchen <hawks.jamesf@gmail.com>
2021-03-02 09:08:16 +0800

    update 200

--- src/basic/_14_DisjointSet_200.py
+++ src/basic/_14_DisjointSet_200.py
@@ -35,6 +35,37 @@ grid[i][j] 的值为 '0' 或 '1'
 '''
 
 
+class UnionFind():
+    def __init__(self, grid):
+        m, n = len(grid), len(grid[0])
+        self.count = 0
+        self.parent = [-1]*(m*n)
+        self.rank = [0]*(m*n)
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == '1':
+                    self.parent[i*n+j] = i*n+j
+                    self.count += 1
+
+    def find(self, i):
+        if self.parent[i] != i:
+            self.parent[i] = self.find(self.parent[i])
+        return self.parent[i]
+
+    def union(self, x, y):
+        rootx = self.find(x)
+        rooty = self.find(y)
+        if rootx != rooty:
+            if self.rank[rootx] > self.rank[rooty]:
+                self.parent[rooty] = rootx
+            elif self.rank[rootx] < self.rank[rooty]:
+                self.parent[rootx] = rooty
+            else:
+                self.parent[rooty] = rootx
+                self.rank[rootx] += 1
+            self.count -= 1
+
+
 class Solution:
     # 类变量
     dx = [-1, 1, 0, 0]
@@ -95,7 +126,6 @@ class Solution:
     def numIslands3(self, grid) -> int:  # disjoinset
         if not grid or not grid[0]:
             return 0
-        from src.common.disjoinset import UnionFind
         uf = UnionFind(grid)
         directions = [
             [0, 1],
