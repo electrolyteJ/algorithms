1d893bbdf4efda207db056e6a021c07da8b7cd85
jamesfchen <hawks.jamesf@gmail.com>
2021-03-10 17:55:46 +0800

    [2021-03-10] add 739

--- src/training/739. Daily Temperatures.py
+++ src/training/739. Daily Temperatures.py
@@ -6,25 +6,32 @@
 
 提示：气温 列表长度的范围是 [1, 30000]。每个气温的值的均为华氏度，都是在 [30, 100] 范围内的整数。
 '''
-
-
 class Solution:
-    def dailyTemperatures(self, T):
+    def dailyTemperatures1(self, T):
+        #时间复杂度O(mn) 空间复杂度O(m)
         n = len(T)
-        ret =[]
-        #时间复杂度O(n*n)
-        for i in range(n):
-            day=0
-            for j in range(i+1,n):
-                if T[i] <T[j]:
-                    day =j-i
-                    break
-            ret.append(day)
-        return ret
+        ans, nxt, big = [0] * n, dict(), 10**9
+        for i in range(n - 1, -1, -1):
+            warmer_index = min(nxt.get(t, big) for t in range(T[i] + 1, 102))
+            if warmer_index != big:
+                ans[i] = warmer_index - i
+            nxt[T[i]] = i
+        return ans
     def dailyTemperatures2(self, T):
+        #时间复杂度O(n) 空间复杂度O(n)
+        length = len(T)
+        ans = [0] * length
+        stack = []
+        for i in range(length):
+            temperature = T[i]
+            while stack and temperature > T[stack[-1]]:
+                prev_index = stack.pop()
+                ans[prev_index] = i - prev_index
+            stack.append(i)
+        return ans
 
 if __name__ == '__main__':
     s = Solution()
     temperatures = [73, 74, 75, 71, 69, 72, 76, 73]
-    print('1', s.dailyTemperatures(temperatures))
+    print('1', s.dailyTemperatures1(temperatures))
     print('2', s.dailyTemperatures2(temperatures))
