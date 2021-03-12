5e122907f1bc67d05335ea504ab9e0a7890038f2
jamesfchen <hawks.jamesf@gmail.com>
2021-03-12 19:53:31 +0800

    [2021-03-12] add 4

--- src/training/4. Median of Two Sorted Arrays.py
+++ src/training/4. Median of Two Sorted Arrays.py
@@ -11,8 +11,7 @@
 输入：nums1 = [1,2], nums2 = [3,4]
 输出：2.50000
 解释：合并数组 = [1,2,3,4] ，中位数 (2 + 3) / 2 = 2.5
-示例 3：                                                                                                                                                                                                                              
-
+示例 3：                                                                           
 输入：nums1 = [0,0], nums2 = [0,0]
 输出：0.00000
 示例 4：
@@ -24,7 +23,6 @@
 输入：nums1 = [2], nums2 = []
 输出：2.00000
  
-
 提示：
 
 nums1.length == m
@@ -41,24 +39,87 @@ nums2.length == n
 
 class Solution:
     def findMedianSortedArrays(self, nums1, nums2) -> float:
+        if not nums1 and not nums2:return 0
+ 
+        def getKthElement(k):
+            index1, index2 = 0, 0
+            while True:
+                # 特殊情况
+                if index1 == m:
+                    return nums2[index2 + k - 1]
+                if index2 == n:
+                    return nums1[index1 + k - 1]
+                if k == 1:
+                    return min(nums1[index1], nums2[index2])
 
-        pass
+                # 正常情况
+                newIndex1 = min(index1 + k // 2 - 1, m - 1)
+                newIndex2 = min(index2 + k // 2 - 1, n - 1)
+                pivot1, pivot2 = nums1[newIndex1], nums2[newIndex2]
+                if pivot1 <= pivot2:
+                    k -= newIndex1 - index1 + 1
+                    index1 = newIndex1 + 1
+                else:
+                    k -= newIndex2 - index2 + 1
+                    index2 = newIndex2 + 1
 
+        m, n = len(nums1), len(nums2)
+        totalLength = m+n
+        if (m+n) &1:
+            return getKthElement((totalLength + 1) // 2)
+        else:
+            return (getKthElement(totalLength // 2) + getKthElement(totalLength // 2 + 1)) / 2
+
+    def findMedianSortedArrays2(self, nums1, nums2) -> float:
+        if len(nums1) > len(nums2):
+            return self.findMedianSortedArrays(nums2, nums1)
+
+        infinty = 2**40
+        m, n = len(nums1), len(nums2)
+        left, right = 0, m
+        # median1：前一部分的最大值
+        # median2：后一部分的最小值
+        median1, median2 = 0, 0
+
+        while left <= right:
+            # 前一部分包含 nums1[0 .. i-1] 和 nums2[0 .. j-1]
+            # // 后一部分包含 nums1[i .. m-1] 和 nums2[j .. n-1]
+            i = (left + right) // 2
+            j = (m + n + 1) // 2 - i
+
+            # nums_im1, nums_i, nums_jm1, nums_j 分别表示 nums1[i-1], nums1[i], nums2[j-1], nums2[j]
+            nums_im1 = (-infinty if i == 0 else nums1[i - 1])
+            nums_i = (infinty if i == m else nums1[i])
+            nums_jm1 = (-infinty if j == 0 else nums2[j - 1])
+            nums_j = (infinty if j == n else nums2[j])
+
+            if nums_im1 <= nums_j:
+                median1, median2 = max(nums_im1, nums_jm1), min(nums_i, nums_j)
+                left = i + 1
+            else:
+                right = i - 1
+
+        return (median1 + median2) / 2 if (m + n) % 2 == 0 else median1
 
 if __name__ == '__main__':
     s = Solution()
     nums1 = [1, 3]
     nums2 = [2]
     print('1', s.findMedianSortedArrays(nums1, nums2))
+    print('2', s.findMedianSortedArrays2(nums1, nums2))
     nums1 = [1, 2]
     nums2 = [3, 4]
     print('1', s.findMedianSortedArrays(nums1, nums2))
+    print('2', s.findMedianSortedArrays2(nums1, nums2))
     nums1 = [0, 0]
     nums2 = [0, 0]
     print('1', s.findMedianSortedArrays(nums1, nums2))
+    print('2', s.findMedianSortedArrays2(nums1, nums2))
     nums1 = []
     nums2 = [1]
     print('1', s.findMedianSortedArrays(nums1, nums2))
+    print('2', s.findMedianSortedArrays2(nums1, nums2))
     nums1 = [2]
     nums2 = []
     print('1', s.findMedianSortedArrays(nums1, nums2))
+    print('2', s.findMedianSortedArrays2(nums1, nums2))
