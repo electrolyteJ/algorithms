11571f5efd8f6ff4e1083be52828d709fa15aaac
jamesfchen <hawks.jamesf@gmail.com>
2021-03-28 22:44:41 +0800

    [2021-03-28] add 97

--- src/training/dp_memoization/97. Interleaving String.py
+++ src/training/dp_memoization/97. Interleaving String.py
@@ -32,16 +32,56 @@ s1、s2、和 s3 都由小写英文字母组成
 
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        pass
+        m,n = len(s1),len(s2)
+        if m+n !=len(s3):
+            return False
+        dp=[[False]*(n+1) for _ in range(m+1)]
+        dp[0][0]=True
+        for i in range(1, m+1):
+            dp[i][0] = dp[i-1][0] and (s1[i-1] == s3[i-1])
+        for j in range(1,n+1):dp[0][j]=dp[0][j-1] and (s2[j-1]==s3[j-1])
+        #时间复杂度O(mn) 空间复杂度O(mn)
+        for i in range(1,m+1):
+            for j in range(1,n+1):
+                p=j+i-1
+                dp[i][j] = (dp[i-1][j] and s1[i-1] == s3[p]) or (dp[i][j-1] and s2[j-1] == s3[p])
+        return dp[m][n]
+
+    def isInterleave2(self, s1: str, s2: str, s3: str) -> bool:#dp空间优化
+        m, n = len(s1), len(s2)
+        if m+n != len(s3):
+            return False
+        dp = [False]*(n+1)
+        dp[0]=True
+        #时间复杂度O(mn) 空间复杂度O(m) 滚动数组优化
+        for i in range( m+1):
+            for j in range(n+1):
+                p = j+i-1
+                if i>0:
+                    dp[j] = dp[j] and s1[i-1] == s3[p]
+                if j >0:
+                    dp[j] = dp[j] or dp[j-1] and s2[j-1] == s3[p]
+        return dp[-1]
+
+
 if __name__ =='__main__':
     s = Solution()
     s1 = "aabcc"; s2 = "dbbca"; s3 = "aadbbcbcac"
     print('1',s.isInterleave(s1,s2,s3))
+    print('2',s.isInterleave2(s1,s2,s3))
     s1 = "aabcc"; s2 = "dbbca"; s3 = "aadbbbaccc"
     print('1',s.isInterleave(s1,s2,s3))
+    print('2',s.isInterleave2(s1,s2,s3))
     s1 = ""; s2 = ""; s3 = ""
     print('1',s.isInterleave(s1,s2,s3))
+    print('2',s.isInterleave2(s1,s2,s3))
     s1="aabd"
     s2="abdc"
     s3="aabdbadc"
     print('1',s.isInterleave(s1,s2,s3))
+    print('2',s.isInterleave2(s1,s2,s3))
+    s1="db"
+    s2="b"
+    s3="cbb"
+    print('1',s.isInterleave(s1,s2,s3))
+    print('2',s.isInterleave2(s1,s2,s3))
