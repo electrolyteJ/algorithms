6944abef9000cf1d6b2e73a6774d15f11768e23f
jamesfchen <hawks.jamesf@gmail.com>
2021-03-30 22:09:27 +0800

    [2021-03-30] add 128

--- src/training/stack_queue_hash/128. Longest Consecutive Sequence.py
+++ src/training/stack_queue_hash/128. Longest Consecutive Sequence.py
@@ -22,9 +22,21 @@
 from typing import List
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-            pass
+        s = set(nums)
+        max_len=0
+        for num in nums:
+            if num-1 not in s:
+                cur_len=1
+                cur_num=num+1
+                while cur_num in s:
+                    cur_num +=1
+                    cur_len+=1
+                max_len=max(max_len,cur_len)
+        return max_len
+
 if __name__ =='__main__':
     s = Solution()
     nums = [100, 4, 200, 1, 3, 2]
     print('1',s.longestConsecutive(nums))
     nums = [0, 3, 7, 2, 5, 8, 4, 6, 0, 1]
+    print('1',s.longestConsecutive(nums))
