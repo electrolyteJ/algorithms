689ea39f28cfb25e4d35e731f255940a01dff67b
jamesfchen <hawks.jamesf@gmail.com>
2021-04-05 15:37:51 +0800

    [2021-04-05] add 480

--- src/training/divide_conquer/53_Maximum_Subarray.py
+++ src/training/divide_conquer/53_Maximum_Subarray.py
@@ -23,43 +23,88 @@
 
 输入：nums = [-100000]
 输出：-100000
- 
+
 
 提示：
 
 1 <= nums.length <= 3 * 104
 -105 <= nums[i] <= 105
- 
+
 
 进阶：如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的 分治法 求解。
 '''
-
+class Status:
+    def __init__(self,lsum,rsum,msum,isum):
+        self.lsum = lsum
+        self.rsum = rsum
+        self.msum = msum
+        self.isum = isum
 
 class Solution:
-    def maxSubArray1(self, nums) -> int:
+    def maxSubArray(self, nums) -> int:  # dp
         if not nums: return 0
-        dp = [0] * 2
-        dp[0], ret = nums[0],nums[0]
-        for i in range(1, len(nums)):
-            a, b = i % 2, (i - 1) % 2
-            dp[a] = max(dp[b] + nums[i], nums[i])
-            ret = max(ret,dp[a])
+        n = len(nums)
+        dp = [0]*n
+        ret = dp[0] = nums[0]
+        # dp[i] 下标为i的最大和
+        for i in range(1, n):
+            dp[i] = max(dp[i-1]+nums[i], nums[i])
+            ret = max(dp[i], ret)
         return ret
-    def maxSubArray2(self, nums) -> int:#分治法
-        pass
+
+    def maxSubArray1(self, nums) -> int:  # dp空间优化
+        # if not nums: return 0
+        # dp = [0] * 2
+        # dp[0], ret = nums[0],nums[0]
+        # for i in range(1, len(nums)):
+        #     a, b = i % 2, (i - 1) % 2
+        #     dp[a] = max(dp[b] + nums[i], nums[i])
+        #     ret = max(ret,dp[a])
+        # return ret
+        if not nums: return 0
+        o = p = ret = nums[0]
+        for i in range(1, len(nums)):
+            p = max(o + nums[i], nums[i])
+            ret = max(ret, p)
+            o = p
+        return ret
+
+    def maxSubArray2(self, nums) -> int:  # 分治法
+        if not nums: return 0
+        #时间复杂度O(n) 空间复杂度O(logn)  线段树
+        def divide(left, right):
+            if left >= right:
+                return Status(nums[left], nums[left], nums[left], nums[left])
+            mid = left + (right-left)//2
+            lsub = divide(left, mid)
+            rsub = divide(mid+1, right)
+            
+            isum = lsub.isum+rsub.isum
+            lsum = max(lsub.lsum,lsub.isum+rsub.lsum)
+            rsum = max(rsub.rsum, lsub.isum+rsub.rsum)
+            msum = max(max(lsub.msum,rsub.msum),lsub.rsum+rsub.lsum)
+            return Status(lsum, rsum, msum, isum)
+
+        return divide(0, len(nums)-1).msum
+         
+
 
 
 if __name__ == '__main__':
-    s = Solution()
-    nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+    s=Solution()
+    nums=[-2, 1, -3, 4, -1, 2, 1, -5, 4]
+    print('0', s.maxSubArray(nums))
     print('1', s.maxSubArray1(nums))
     print('2', s.maxSubArray2(nums))
-    nums = [1]
+    nums=[1]
+    print('0', s.maxSubArray(nums))
     print('1', s.maxSubArray1(nums))
     print('2', s.maxSubArray2(nums))
-    nums = [0]
+    nums=[0]
+    print('0', s.maxSubArray(nums))
     print('1', s.maxSubArray1(nums))
     print('2', s.maxSubArray2(nums))
-    nums = [-1]
+    nums=[-1]
+    print('0', s.maxSubArray(nums))
     print('1', s.maxSubArray1(nums))
     print('2', s.maxSubArray2(nums))
--- src/training/sliding_window/480. Sliding Window Median.py
+++ src/training/sliding_window/480. Sliding Window Median.py
@@ -27,48 +27,87 @@
 与真实值误差在 10 ^ -5 以内的答案将被视作正确答案。
 '''
 
+import collections
 import heapq
+
+
 class MedianFinder:
-
-    def __init__(self,k):
-        """
-        initialize your data structure here.
-        """
-        self.minheap = []
-        self.maxheap = []
-        import collections
+    def __init__(self, k):
+        self.k = k
+        self.small = []
+        self.large = []
+        self.small_size = self.large_size = 0
         self.delayed = collections.Counter()
-        self.k=k
 
-    #时间复杂度O(logn)
-    def addNum(self, num: int) -> None:
-        heapq.heappush(self.minheap, num)
-        heapq.heappush(self.maxheap, -heapq.heappop(self.minheap))
-        if len(self.minheap) < len(self.maxheap):
-            n = -heapq.heappop(self.maxheap)
-            heapq.heappush(self.minheap, n)
+    def prune(self, heap):
+        while heap:
+            num = heap[0]
+            if heap is self.small:
+                num = -num
+            if num in self.delayed:
+                self.delayed[num] -= 1
+                if self.delayed[num] == 0:
+                    self.delayed.pop(num)
+                heapq.heappop(heap)
+            else:
+                break
 
-    def findMedian(self) -> float:
-        return (self.minheap[0]-self.maxheap[0])/2 if len(self.minheap) == len(self.maxheap) else self.minheap[0]
-    def popNum(self,num):
-        # heapq.heappop()
-        pass
+    def make_balance(self):
+        if self.small_size >= self.large_size + 2:
+            heapq.heappush(self.large, -self.small[0])
+            heapq.heappop(self.small)
+            self.small_size -= 1
+            self.large_size += 1
+            self.prune(self.small)
+        elif self.large_size >= self.small_size+1:
+            heapq.heappush(self.small, -self.large[0])
+            heapq.heappop(self.large)
+            self.small_size += 1
+            self.large_size -= 1
+            self.prune(self.large)
+
+    def add_num(self, num):
+        if not self.small or num < -self.small[0]:
+            heapq.heappush(self.small, -num)
+            self.small_size += 1
+        else:
+            heapq.heappush(self.large, num)
+            self.large_size += 1
+        self.make_balance()
+
+    def pop_num(self, num):
+        self.delayed[num] += 1
+        if num <= -self.small[0]:
+            self.small_size -= 1
+            if num == -self.small[0]:
+                self.prune(self.small)
+        else:
+            self.large_size -= 1
+            if num == self.large[0]:
+                self.prune(self.large)
+        self.make_balance()
+
+    def find_median(self):
+        return float(-self.small[0]) if self.k % 2 == 1 else (-self.small[0]+self.large[0])/2
+from typing import List
 
 class Solution:
-    def medianSlidingWindow(self, nums, k: int):
-        if not nums:return
-        m = MedianFinder(k)
-        ret=[]
-        for i in range(k-1):
-            m.addNum(nums[i])
-        #时间复杂度O(n*logn)
-        for i in range(k-1,len(nums)):
-            m.addNum(nums[i])
-            ret.append(m.findMedian())
-            # m.popleft()
+    def medianSlidingWindow(self, nums: List[int], k: int) -> List[float]:
+        if not nums:
+            return []
+        f = MedianFinder(k)
+        for i in range(k):
+            f.add_num(nums[i])
+        ret = [f.find_median()]
+        for i in range(k, len(nums)):
+            f.add_num(nums[i])
+            f.pop_num(nums[i-k])
+            ret.append(f.find_median())
         return ret
-        
-if __name__ =='__main__':
+
+
+if __name__ == '__main__':
     s = Solution()
-    nums = [1, 3, -1, -3, 5, 3, 6, 7]; k = 3
+    nums = [1, 3, -1, -3, 5, 3, 6, 7]
+    k = 3
     print('1', s.medianSlidingWindow(nums, k))  # [1,-1,-1,3,5,6]
