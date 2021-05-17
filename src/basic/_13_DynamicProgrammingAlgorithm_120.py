8d013898bba665688788a91f4e7978fb7fedbafc
jamesfchen <hawks.jamesf@gmail.com>
2021-05-17 22:43:13 +0800

    update

--- src/basic/_13_DynamicProgrammingAlgorithm_120.py
+++ src/basic/_13_DynamicProgrammingAlgorithm_120.py
@@ -32,7 +32,7 @@ class Solution:
     def minimumTotal1(self, triangle) -> int:  # dp
         if not triangle:
             return 0
-        res = triangle[-1].copy()
+        res = triangle[-1][:]
         #时间复杂度为O(m*n)
         for i in range(len(triangle)-2, -1, -1):
             for j in range(len(triangle[i])):
